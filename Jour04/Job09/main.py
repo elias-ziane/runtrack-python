7d59f6c52d00c9b6d570e27cbfdf38023a4abe28
elias-ziane <elias-ziane@github.com>
2023-11-23 14:53:15 +0100

    Job09 done

---
+++ Jour04/Job09/main.py
@@ -0,0 +1,8 @@
+L = [8, 24, 27, 48, 2,16, 9, 102, 7, 84, 91]
+
+valeur_maximum=max(L)
+print("La valeur max est: ",valeur_maximum)
+
+valeur_minimale=min(L)
+print("La valeur min est: ",valeur_minimale)
+
