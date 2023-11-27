f287d2d0da4680adc2df9d637a257a391ea1817d
elias-ziane <elias-ziane@github.com>
2023-11-27 10:41:36 +0100

    Job02 done

---
+++ Jour05/Job02/main.py
@@ -0,0 +1,13 @@
+def draw_rectangle(width, height):
+    # Dessine la partie supérieure du rectangle
+    print('-' * width)
+
+    # Dessine les côtés du rectangle
+    for _ in range(height - 2):
+        print('|' + ' ' * (width - 2) + '|')
+
+    # Dessine la partie inférieure du rectangle
+    print('-' * width)
+
+# Exemple d'utilisation
+draw_rectangle(10, 5)
